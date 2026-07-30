7fed1096886ea7240a7c82f9c36bdb152aad490b
Jonas <jonashausmann@icloud.com>
2026-07-30 10:42:13 -0700

    script for creating test data

---
+++ create_test_data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# This creates a straight filament for testing data
+nLoops = 1000
+monomers = 50
+
+
+
+
+
+
+
+
+
+
+coord_rows = []
+i = 0
+while i < nLoops:
+    monomer = 1
+    while monomer < monomers+1:
+        row = [i,monomer,0,monomer-1]
+        coord_rows.append(row)
+        monomer = monomer+1
+    i = i + 1
+df = pd.DataFrame(coord_rows, columns=["frame","monomernumber","xcoord","ycoord"])
+df.to_csv("straight_filament_test.csv", index=False)
--- xyz_to_csv_conversion.py
+++ xyz_to_csv_conversion.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import re
 # This is just a reminder to myself that this only works for the free polymer case
 # particle_type = 2
 
@@ -29,6 +30,7 @@ def xyz_to_csv(xyz_filename, parameters_filename):
                     param_string = value.strip()
                     param_value = float(param_string)
                     parameter_dict.update({parameter : param_value})
+    # creating the parameters.py file with my new parameters!
     with open("parameters.py", "w") as f:
         for key, value in parameter_dict.items():
             f.write(f"{key} = {value}\n")
@@ -46,6 +48,7 @@ def xyz_to_csv(xyz_filename, parameters_filename):
     while is_there_already_a_csv_file == True:
         runNumber = runNumber + 1
         name_of_csv_file = name_of_csv_file.replace(".csv","")
+        name_of_csv_file = re.sub(r"_Run/d+", name_of_csv_file)
         name_of_csv_file = name_of_csv_file + "_Run" + str(runNumber) + ".csv"
         is_there_already_a_csv_file = os.path.exists(name_of_csv_file)
 
@@ -76,7 +79,7 @@ def xyz_to_csv(xyz_filename, parameters_filename):
     df["xcoord"] = df["xcoord"].astype(float)
     df["ycoord"] = df["ycoord"].astype(float)
     df["zcoord"] = df["zcoord"].astype(float)
-    df.drop(['particletype','zcoord'], axis=1,inplace=True)
+    df.drop(['linenumber','particletype','zcoord'], axis=1,inplace=True)
     df.to_csv(name_of_csv_file, index=False)
 
 
