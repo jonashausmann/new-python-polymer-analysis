ca526988b574371ae3a2e5c49fc50a4642948875
Jonas <jonashausmann@icloud.com>
2026-07-31 14:56:59 -0700

    getting error where x_2[0] is out of bounds, likely due to monomers+1, but need to figure out why

--- curvature_mean_max.py
+++ curvature_mean_max.py
@@ -12,8 +12,13 @@ def calculate_curvature(coordinate_csv, measurement_csv):
 
     
 
+    monomer_curvatures = []
     mean_curv_rows = []
     max_curv_rows = []
+    max_monomer_rows = []
+    min_curv_rows = []
+    min_monomer_rows = []
+    
     while frame < nLoop:
         monomer = 1
         while monomer < monomers-1:
@@ -21,8 +26,27 @@ def calculate_curvature(coordinate_csv, measurement_csv):
             y_1 = df[(df["frame"] == frame) & (df["monomernumber"] == monomer)]["ycoord"]
             x_2 = df[(df["frame"] == frame) & (df["monomernumber"] == monomer + 1)]["xcoord"]
             y_2 = df[(df["frame"] == frame) & (df["monomernumber"] == monomer + 1)]["ycoord"]
-            x = float(x_2.iloc[0]) -float(x_1.iloc[0])
+            if frame == nLoop -1:
+                print(f"Monomer: {Monomer}, Frame: {frame}, x: {x_2.iloc[0]}")
+            x = float(x_2.iloc[0]) - float(x_1.iloc[0])
             y = float(y_2.iloc[0]) - float(y_1.iloc[0])
             theta = np.arctan2(x,y)
+            monomer_curvatures.append(theta)
+            monomer = monomer + 1
+        frame_mean = [frame, np.mean(monomer_curvatures)]
+        mean_curv_rows.append(frame_mean)
 
+        frame_max = [frame, monomer_curvatures.index(np.max(monomer_curvatures)), np.max(monomer_curvatures)]
+        max_curv_rows.append(frame_max)
 
+        frame_min = [frame, monomer_curvatures.index(np.min(monomer_curvatures)),np.min(monomer_curvatures)]
+        min_curv_rows.append(frame_min)
+
+        frame = frame + 1
+
+    df["frame","mean_curvature"] = mean_curv_rows
+    df["frame","max_curv_bead_index","max_curv"] = max_curv_rows
+    df["frame", "min_curv_bead_index", "min_curv"] = min_curv_rows
+    print(np.mean(df["mean_curvature"]))
+
+calculate_curvature("./csv_files/F5.0_K0.5_theta0.0.csv", "./calculated_data/real_polymer_test.csv")
