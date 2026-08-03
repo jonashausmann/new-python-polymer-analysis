db723d736b7d7d5f2cb39c94a69ceb1a0cea8b8f
Jonas <jonashausmann@icloud.com>
2026-08-03 14:25:13 -0700

    turning number script works for test data

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,5 +1,5 @@
 from . import curvature_mean_max
 from . import direction_of_head_bead
-from . import parameters
 from . import rg_calculation
-from . import xyz_to_csv_conversion.py
+from . import xyz_to_csv_conversion
+from . import parameters
--- turning_number.py
+++ turning_number.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import analysis
+#import analysis
 
 #monomers = analysis.parameters.monomers 
 #frames = anaylsis.parameters.nLoop/1000
@@ -14,42 +14,50 @@ monomers = 7
 
 
 
-def_calculate_turning_number(coordinate_csv, measurement_csv):
-    coord_df = pd.read_csv(coordinate_csv)
+def calculate_turning_number(coordinate_csv, measurement_csv):
+    df = pd.read_csv(coordinate_csv)
     measurement_df = pd.read_csv(measurement_csv)
     frame = 0
     turning_number_row = []
     while frame < frames:
-        monomer = 0
+        monomer = 1
         frame_phi_angles = []
-        for monomer in monomers-1:
+        while monomer < monomers:
             x_1 = df[(df["monomernumber"] == monomer)]["xcoord"]
             y_1 = df[(df["monomernumber"] == monomer)]["ycoord"]
             x_2 = df[(df["monomernumber"] == monomer + 1)]["xcoord"]
             y_2 = df[(df["monomernumber"] == monomer + 1)]["ycoord"]
             x = float(x_2.iloc[0]) - float(x_1.iloc[0])
             y = float(y_2.iloc[0]) - float(y_1.iloc[0])
-            monomer_vec = np.array(x,y)
-            monomer_unit_vec = monomer_vec / np.linalg.norm(monomer_vec)
             phi_angle = np.arctan2(y, x)
             frame_phi_angles.append(phi_angle)
+            monomer = monomer + 1
+            print(f"Monomer:{monomer} Phi Angle:{phi_angle}")
         i = 0
         phi_differences = []
         while i < (len(frame_phi_angles)-1):
             monomer_phi_difference = frame_phi_angles[i+1] - frame_phi_angles[i]
+            monomer_phi_difference = (monomer_phi_difference*180)/np.pi
             phi_differences.append(monomer_phi_difference)
+            print(f"Angle difference {(monomer_phi_difference*180)/np.pi}")
             i = i + 1
         sum_of_phi = 0
         for phi_difference in phi_differences:
+            print(phi_difference)
+            if phi_difference <= -180:
+                phi_difference = phi_difference + 360
+                print(f"I'm a changed phi_difference! {phi_difference}")
+            elif phi_difference > 180:
+                phi_difference = phi_difference - 360
             sum_of_phi = sum_of_phi + phi_difference
-        turning_number = sum_of_phi/(2*np.pi)
-        turning_number_degrees = (turning_number*180)/np.pi
-        turning_number_row.append(turning_number_degrees)
+        turning_number = sum_of_phi/(360)
+        turning_number_row.append(turning_number)
+        frame = frame + 1
     measurement_df["turning_number"] = turning_number_row
 
     measurement_df.to_csv(measurement_csv, index=False)
 
-def_calculate_turning_number("./data/writhe_data_test.csv","./calculated_data/turning_number_test.csv")
+calculate_turning_number("./data/writhe_data_test.csv","./calculated_data/turning_number_test.csv")
 
 
 
