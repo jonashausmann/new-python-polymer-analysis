8f059cc8ef929960a0cd6fc4f9275f218d1852e5
Jonas <jonashausmann@icloud.com>
2026-08-04 16:45:49 -0700

    it works, somehow

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import analysis
 from analysis import parameters
 
 def main(simulation_directory):
+    '''
     simulation_directory = str(simulation_directory)
 
     frames = parameters.nLoop / 1000
@@ -32,7 +33,7 @@ def main(simulation_directory):
     print("Winding Number Calculated")
     analysis.density.compute_density(measurement_csv_name)
     print("Density Calculated")
-
+'''
 '''
 
 main("/Users/jonashausmann/workspaces/github.com/ucmerced/new-python-polymer-analysis/data/F05_K0.5_theta0.0/RUN_0001/")
@@ -45,20 +46,21 @@ def find_directories_and_run_for_all():
     for subdir in subdirs:
         resolved_subdir = Path(subdir).resolve()
         subsubdirs = [p for p in resolved_subdir.iterdir() if p.is_dir()]
+        subsubdirs = [p for p in subsubdirs if "RUN" in p.name]
         for subsubdir in subsubdirs:
             print(f"Starting Initial Calculations for {subsubdir}")
             main(subsubdir)
             print(f"Finished initial Calculation for {subsubdir}")
         sort_subsubdirs = sorted(subsubdirs, key=lambda x: int(x.name.split('_')[1]))
 
-        run_average_directory = str(subdir) + "/Averaged_Runs/"
-        os.mkdir(run_average_directory)
-        run_avg_resolved = Path(run_average_directory).resolve()
-        average_df = pd.DataFrame({"run" : np.arange(len(subsubdirs))})
-        average_csv_name = str(run_avg_resolved) + "/average.csv"
-        average_df.to_csv(average_csv_name, index=False)
+#        average_df = pd.DataFrame({"run" : np.arange(len(subsubdirs))})
+#        average_csv_name = str(run_avg_resolved) + "/average.csv"
+        #average_df.to_csv(average_csv_name, index=False)
 
         run_number = 1
+        variables_to_average_min_max = ["Rg","turning_number",
+                                        "winding_number","density"]
+        variable_dict = {}
         for subsubdir in sort_subsubdirs:
             print(f"Starting average calculations for run {run_number}")
             csv_folder = str(subsubdir) + "/csv_files/"
@@ -67,6 +69,39 @@ def find_directories_and_run_for_all():
             csv_file_name = str(csv_file[0])
             measurement_df = pd.read_csv(csv_file_name)
 
+            variable_dict.setdefault("run", []).append(run_number)
+
+            for variable in variables_to_average_min_max:
+                variable_mean = variable + "_mean"
+                variable_max = variable + "_max"
+                variable_min = variable + "_min"
+
+                variable_dict.setdefault(variable_mean,[]).append(np.mean(measurement_df[variable]))
+                variable_dict.setdefault(variable_max,[]).append(np.max(measurement_df[variable]))
+                variable_dict.setdefault(variable_min,[]).append(np.min(measurement_df[variable]))
+
+            variable_dict.setdefault("direction_mean",[]).append(np.mean(measurement_df["degrees"]))
+            variable_dict.setdefault("mean_curv_mean",[]).append(np.mean(measurement_df["mean_curv"]))
+            variable_dict.setdefault("max_curv_max",[]).append(np.max(measurement_df["max_curv"]))
+            variable_dict.setdefault("min_curv_min",[]).append(np.min(measurement_df["min_curv"]))
+
+            variable_dict.setdefault("nLoop",[]).append(parameters.nLoop/1000)
+            variable_dict.setdefault("bending",[]).append(parameters.bending)
+            variable_dict.setdefault("activity",[]).append(parameters.activity)
+            variable_dict.setdefault("theta",[]).append(parameters.ChiralityAngle)
+            run_number = run_number + 1
+
+        average_df = pd.DataFrame(variable_dict)
+        run_average_directory = str(subdir) + "/Averaged_Runs/"
+        os.mkdir(run_average_directory)
+        run_avg_resolved = Path(run_average_directory).resolve()
+        average_csv_name = str(run_avg_resolved) + "/average.csv"
+        average_df.to_csv(average_csv_name, index=False)
+        print(f"Calculations Complete for {run_number}")
+
+
+
+'''
             average_rg = np.mean(measurement_df["Rg"])
             average_df["average_rg"] = average_rg
             min_rg = np.min(measurement_df["Rg"])
@@ -109,14 +144,11 @@ def find_directories_and_run_for_all():
             average_df["bending"] = parameters.bending
             average_df["activity"] = parameters.activity
             average_df["theta"] = parameters.ChiralityAngle
+'''
 
 
 
 
-
-            average_df.to_csv(average_csv_name, index=False)
-            print(f"Calculations Complete for {run_number}")
-            run_number = run_number + 1
 
         
         
