06e9cecbe0154dff08e11e2bdae2ca31135f1a13
Jonas <jonashausmann@icloud.com>
2026-08-07 11:05:13 -0700

    updated critical point counting function, it works

--- analysis/times_variable_goes_over_quantity.py
+++ analysis/times_variable_goes_over_quantity.py
@@ -11,16 +11,17 @@ def count_surpass_critical_value(value, critical_point, measurements_csv, run_av
 
 
 
-
-    if measurement_df[(measurement_df["frames"] == 0)][value] > critical_point:  
+    
+    if measurement_df[(measurement_df["frames"] == 0)][value].iloc[0] > critical_point:  
         amount_of_times = (measurement_df[value] >= critical_point).sum()
         value_count_string = value + "_times_over_" + str(critical_point)
     else:
         amount_of_times = (measurement_df[value] <= critical_point).sum()
-        value_count_string = value + "_times_over_" + str(critical_point)
+        value_count_string = value + "_times_under_" + str(critical_point)
 
     
 
+    print(value_count_string)
     print(amount_of_times)
 
 '''
@@ -30,9 +31,9 @@ def count_surpass_critical_value(value, critical_point, measurements_csv, run_av
 def count_multiple_variables(measurements_csv_path, run_averages_csv):
     variables = {
             "Rg" : 10.0,
-            "mean_curv" : 100,
+            "mean_curv" : 6,
             }
     for variable, critical_value in variables.items():
-        count_surpass_critical_value(variable, critical_value, measurements_csv_path, run_averages_csv)
+        count_surpass_critical_value(variable, critical_value, measurements_csv_path,run_averages_csv)
 
 count_multiple_variables("../data/F10_K5_theta0.0/RUN_0001/csv_files/F10.0_K5.0_theta0.0_measurements.csv", "../data/F10_K5_theta0.0/Averaged_Runs/average.csv")
