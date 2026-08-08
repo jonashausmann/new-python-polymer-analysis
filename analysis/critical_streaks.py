1af22832d91cc7da51c14ee32e53eb8207000407
Jonas <jonashausmann@icloud.com>
2026-08-07 17:17:59 -0700

    created new analysis tools

---
+++ analysis/critical_streaks.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import parameters
+
+def count_critical_value_streak_greater_than(measurement_df, critical_variable, critical_value):
+    df = pd.read_csv(measurement_df)
+    frames = int(parameters.nLoop/parameters.pInterval)
+    frame = 0
+    streak = 0
+    streaks = []
+    
+
+
+
+
+    while frame < frames:
+        frame_df = df[(df["frames"] == frame)]
+        if float(frame_df[critical_variable].iloc[0]) >= critical_value:
+            if len(streaks) != 0:
+                streak = streak + 1
+            else:
+                streak = 1
+        else:
+            streak = 0
+        streaks.append(streak)
+        frame = frame + 1
+    streak_name = critical_variable + "_over_" + str(critical_value) + "_streak"
+    df[streak_name] = streaks
+    df.to_csv(measurement_df, index=False)
+
+def count_critical_value_streak_less_than(measurement_df, critical_variable, critical_value):
+    df = pd.read_csv(measurement_df)
+    frames = int(parameters.nLoop/parameters.pInterval)
+    frame = 0
+    streak = 0
+    streaks = []
+    
+
+
+
+
+    while frame < frames:
+        frame_df = df[(df["frames"] == frame)]
+        if float(frame_df[critical_variable].iloc[0]) <= critical_value:
+            if len(streaks) != 0:
+                streak = streak + 1
+            else:
+                streak = 1
+        else:
+            streak = 0
+        streaks.append(streak)
+        frame = frame + 1
+    streak_name = critical_variable + "_under_" + str(critical_value) + "_streak"
+    df[streak_name] = streaks
+    df.to_csv(measurement_df, index=False)
+
+
+
